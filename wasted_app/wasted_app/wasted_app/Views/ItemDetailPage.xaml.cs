729d011b08820355b3dc439b69cf0ed2c7b74b88
Jokubas Vaisnoras <jokkbas@gmail.com>
2021-09-12 20:29:04 +0300

    adding wasted project app

---
+++ wasted_app/wasted_app/wasted_app/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using wasted_app.ViewModels;
+using Xamarin.Forms;
+
+namespace wasted_app.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ wasted_app/wasted_app/wasted_app/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wasted_app.Models;
+using wasted_app.ViewModels;
+using wasted_app.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace wasted_app.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
