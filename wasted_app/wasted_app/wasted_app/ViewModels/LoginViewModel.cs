a2d3b6ccfeb3c0d5909f996c6ebbd32d538928bd
KristupasNo <76537530+KristupasNo@users.noreply.github.com>
2021-09-16 19:12:13 +0300

    Update LoginViewModel.cs
    laikinas loginas

--- wasted_app/wasted_app/wasted_app/ViewModels/LoginViewModel.cs
+++ wasted_app/wasted_app/wasted_app/ViewModels/LoginViewModel.cs
@@ -3,22 +3,57 @@ using System.Collections.Generic;
 using System.Text;
 using wasted_app.Views;
 using Xamarin.Forms;
+using System.ComponentModel;
+using System.Windows.Input;
 
 namespace wasted_app.ViewModels
 {
-    public class LoginViewModel : BaseViewModel
+    public class LoginViewModel : INotifyPropertyChanged //BaseViewModel
     {
+        public Action DisplayInvalidLoginPrompt;
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Email"));
+            }
+        }
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Password"));
+            }
+        }
+        public ICommand SubmitCommand { set; get; }
         public Command LoginCommand { get; }
 
         public LoginViewModel()
         {
-            LoginCommand = new Command(OnLoginClicked);
+           // LoginCommand = new Command(OnLoginClicked);
+            SubmitCommand = new Command(OnSubmit);
         }
 
-        private async void OnLoginClicked(object obj)
+        public async void OnSubmit(object obj)
         {
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+
+            if (email != "admin" || password != "admin")
+            {
+                DisplayInvalidLoginPrompt();
+            }
+            else
+            {
+
+                await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            }
         }
     }
 }
