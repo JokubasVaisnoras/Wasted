d51958712027ec682ebef0496a35b881db984ace
Jokubas Vaisnoras <jokkbas@gmail.com>
2021-09-16 18:17:17 +0300

    adding old project back

---
+++ wasted_app/wasted_app/wasted_app/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using wasted_app.Models;
+using wasted_app.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace wasted_app.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
