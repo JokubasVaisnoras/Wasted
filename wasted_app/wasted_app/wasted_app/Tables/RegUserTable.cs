c039551639b0b66ab1d0b064db3f490b60de846a
KristupasNo <76537530+KristupasNo@users.noreply.github.com>
2021-09-23 23:01:27 +0300

    Create RegUserTable.cs

---
+++ wasted_app/wasted_app/wasted_app/Tables/RegUserTable.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wasted_app.Tables
+{
+    public class RegUserTable
+    {
+        public Guid UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
