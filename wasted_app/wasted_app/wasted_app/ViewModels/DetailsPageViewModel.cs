c6676b58de44868ea288a3506f7f5244c25734c3
KristupasNo <76537530+KristupasNo@users.noreply.github.com>
2021-09-23 23:13:54 +0300

    Add files via upload
    Logout button in user detail settings

---
+++ wasted_app/wasted_app/wasted_app/ViewModels/DetailsPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+using wasted_app.Views;
+using Xamarin.Forms;
+
+
+namespace wasted_app.ViewModels
+{
+    public class DetailsPageViewModel 
+    {
+        public INavigation _navigation;
+        public ICommand LogoutCommand
+        {
+            get;
+            private set;
+        }
+        public DetailsPageViewModel(INavigation navigation)
+        {
+            _navigation = navigation;
+            LogoutCommand = new Command(() => ResetUserInfo());
+        }
+        void ResetUserInfo()
+        {
+            _navigation.PushAsync(new LoginPage());
+            
+        }
+    }
+}
