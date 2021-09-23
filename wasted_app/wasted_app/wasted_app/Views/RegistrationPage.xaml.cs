4792da5ee2f43cd4ca4d21d9bfc0ed411365dcf3
KristupasNo <76537530+KristupasNo@users.noreply.github.com>
2021-09-23 22:56:28 +0300

    Add files via upload

---
+++ wasted_app/wasted_app/wasted_app/Views/RegistrationPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using SQLite;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using wasted_app.Tables;
+
+namespace wasted_app.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "UserDatabase.db");
+            var db = new SQLiteConnection(dbpath);
+            db.CreateTable<RegUserTable>();
+
+            var item = new RegUserTable()
+            {
+                Username = EntryUsername.Text,
+                Password = EntryUserPassword.Text,
+                Email = EntryUserEmail.Text,
+                PhoneNumber = EntryUserPhoneNumber.Text
+
+            };
+
+            db.Insert(item);
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var result = await this.DisplayAlert("Congratulations!", "User is registered", "Yes", "Cancel");
+
+                if (result)
+                    await Navigation.PushAsync(new LoginPage());
+            });
+        }
+    }
+}
