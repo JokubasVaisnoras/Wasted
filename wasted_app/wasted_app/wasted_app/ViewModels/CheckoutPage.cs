af4a07f6844892bdc41489a6524c5ff7037ba679
TadasSabestinas <90024877+TadasSabestinas@users.noreply.github.com>
2021-09-23 21:07:04 +0300

    Add files via upload

---
+++ wasted_app/wasted_app/wasted_app/ViewModels/CheckoutPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace wasted_app.ViewModels
+{
+    public class CheckoutPage : BaseViewModel
+    {
+        public CheckoutPage()
+        {
+            Title = "Checkout";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://www.paypal.com/lt/home"));
+        }
+        public ICommand OpenWebCommand { get; }
+    }
+}
