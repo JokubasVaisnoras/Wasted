824facb2db9cdd37e0b8d9d47ce0ad5a02cfce79
TadasSabestinas <90024877+TadasSabestinas@users.noreply.github.com>
2021-11-05 00:59:22 +0200

    Update PaymentPage.xaml.cs

--- wasted_app/wasted_app/wasted_app/Views/PaymentPage.xaml.cs
+++ wasted_app/wasted_app/wasted_app/Views/PaymentPage.xaml.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Stripe;
 
 namespace wasted_app.Views
 {
@@ -15,6 +16,10 @@ namespace wasted_app.Views
         public PaymentPage()
         {
             InitializeComponent();
+        }
+        private async void Button_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new PaymentSucessPage());
         }
     }
 }
