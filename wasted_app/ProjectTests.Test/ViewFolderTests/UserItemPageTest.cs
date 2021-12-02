39634e0389599f975b331baf15444b573390da4a
Jokubas Vaisnoras <jokkbas@gmail.com>
2021-12-03 00:30:21 +0200

    adding unit tests

---
+++ wasted_app/ProjectTests.Test/ViewFolderTests/UserItemPageTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using wasted_app.Views;
+
+namespace ProjectTests.Test.ViewFolderTests
+{
+    [TestFixture]
+    class UserItemPageTest
+    {
+        [Test]
+        public void UserItemPage_SearchBar_Test()
+        {
+            var userItem = new UserItemPage();
+           
+        }
+    }
+}
